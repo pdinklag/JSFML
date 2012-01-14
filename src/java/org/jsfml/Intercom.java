6a5bbe687b4309038f5c265239d12f4886d2e9fc
pdinklag <pdinklag@googlemail.com>
2012-01-14 11:31:13 +0100

    Added Intercom annotation to mark intercom critical types, fields and constructors
    Signed-off-by: pdinklag <pdinklag@googlemail.com>

---
+++ src/java/org/jsfml/Intercom.java
@@ -0,0 +1,14 @@
+package org.jsfml;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation for intercom-critical types, fields and constructors.
+ * <p/>
+ * This annotation is purely for information purposes and provides no actual functionality. Elements
+ * annotated by this annotation must not be refactored without altering the respective C++ sources.
+ */
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.CONSTRUCTOR})
+public @interface Intercom {
+}
