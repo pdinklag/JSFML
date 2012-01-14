89145a45fcf7fdd8203e6e22e44ef35880359beb
pdinklag <pdinklag@googlemail.com>
2012-01-14 10:02:37 +0100

    Fixed typo in package name
    Signed-off-by: pdinklag <pdinklag@googlemail.com>

--- src/cpp/Intercom/NativeObject.cpp
+++ src/cpp/Intercom/NativeObject.cpp
@@ -3,7 +3,7 @@
 jfieldID JSFML::NativeObject::f_ptr = 0;
 
 void JSFML::NativeObject::Init(JNIEnv* env) {
-    jclass cls = env->FindClass("org/sfml/SFMLNativeObject");
+    jclass cls = env->FindClass("org/jsfml/SFMLNativeObject");
     if(cls) {
         f_ptr = env->GetFieldID(cls, "ptr", "J");
         env->DeleteLocalRef(cls);
