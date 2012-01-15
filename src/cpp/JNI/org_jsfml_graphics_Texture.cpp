b61301e497443db62e44d548cf20fb996125c399
pdinklag <pdinklag@googlemail.com>
2012-01-15 14:43:33 +0100

    Added Texture class
    Signed-off-by: pdinklag <pdinklag@googlemail.com>

---
+++ src/cpp/JNI/org_jsfml_graphics_Texture.cpp
@@ -0,0 +1,121 @@
+#include <JSFML/JNI/org_jsfml_graphics_Texture.h>
+
+#include <JSFML/Intercom/IntRect.hpp>
+#include <JSFML/Intercom/JavaEnum.hpp>
+#include <JSFML/Intercom/NativeObject.hpp>
+
+#include <SFML/Graphics/Texture.hpp>
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    nativeCreate
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_org_jsfml_graphics_Texture_nativeCreate (JNIEnv *env, jobject obj) {
+    return (jlong)new sf::Texture();
+}
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    nativeCopy
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_org_jsfml_graphics_Texture_nativeCopy (JNIEnv *env, jobject obj) {
+    return (jlong)new sf::Texture(*THIS(sf::Texture));
+}
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    create
+ * Signature: (II)Z
+ */
+JNIEXPORT jboolean JNICALL Java_org_jsfml_graphics_Texture_create (JNIEnv *env, jobject obj, jint width, jint height) {
+    return THIS(sf::Texture)->Create(width, height);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    loadFromMemory
+ * Signature: ([BLorg/jsfml/graphics/IntRect;)Z
+ */
+JNIEXPORT jboolean JNICALL Java_org_jsfml_graphics_Texture_loadFromMemory (JNIEnv *env, jobject obj, jbyteArray arr, jobject area) {
+    std::size_t n = (std::size_t)env->GetArrayLength(arr);
+    jbyte* mem = env->GetByteArrayElements(arr, 0);
+
+    jboolean result = THIS(sf::Texture)->LoadFromMemory(mem, n, JSFML::IntRect::ToSFML(env, area));
+
+    env->ReleaseByteArrayElements(arr, mem, 0);
+    return result;
+}
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    getWidth
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_org_jsfml_graphics_Texture_getWidth (JNIEnv *env, jobject obj) {
+    return THIS(sf::Texture)->GetWidth();
+}
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    getHeight
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_org_jsfml_graphics_Texture_getHeight (JNIEnv *env, jobject obj) {
+     return THIS(sf::Texture)->GetHeight();
+ }
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    nativeUpdate
+ * Signature: (Lorg/jsfml/window/Window;II)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Texture_nativeUpdate (JNIEnv *env, jobject obj, jobject window, jint x, jint y) {
+    THIS(sf::Texture)->Update(*JSFML::NativeObject::GetPointer<sf::Window>(env, window), x, y);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    nativeBind
+ * Signature: (Lorg/jsfml/graphics/Texture$CoordinateType;)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Texture_nativeBind (JNIEnv *env, jobject obj, jobject coordType) {
+    THIS(sf::Texture)->Bind((sf::Texture::CoordinateType)JavaEnum::ordinal(env, coordType));
+}
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    setSmooth
+ * Signature: (Z)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Texture_setSmooth (JNIEnv *env, jobject obj, jboolean b) {
+    THIS(sf::Texture)->SetSmooth(b);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    isSmooth
+ * Signature: ()Z
+ */
+JNIEXPORT jboolean JNICALL Java_org_jsfml_graphics_Texture_isSmooth (JNIEnv *env, jobject obj) {
+    return THIS(sf::Texture)->IsSmooth();
+}
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    setRepeated
+ * Signature: (Z)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Texture_setRepeated (JNIEnv *env, jobject obj, jboolean b) {
+  THIS(sf::Texture)->SetRepeated(b);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Texture
+ * Method:    isRepeated
+ * Signature: ()Z
+ */
+JNIEXPORT jboolean JNICALL Java_org_jsfml_graphics_Texture_isRepeated (JNIEnv *env, jobject obj) {
+    return THIS(sf::Texture)->IsRepeated();
+}
---
+++ src/java/org/jsfml/graphics/Texture.java
@@ -0,0 +1,238 @@
+package org.jsfml.graphics;
+
+import org.jsfml.SFMLNativeObject;
+import org.jsfml.UnsafeOperations;
+import org.jsfml.window.Window;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Immutable 2D texture stored on the graphics card for rendering.
+ */
+public class Texture extends SFMLNativeObject {
+    /**
+     * Types of texture coordinates that can be used for rendering.
+     */
+    public static enum CoordinateType {
+        /**
+         * Normalized OpenGL coordinates ranging from 0 to 1.
+         */
+        NORMALIZED,
+
+        /**
+         * Pixel coordinates ranging from 0 to the respective dimension (width or height).
+         */
+        PIXELS
+    }
+
+    /**
+     * Creates a texture.
+     */
+    public Texture() {
+        super();
+    }
+
+    @SuppressWarnings("deprecation")
+    private Texture(long wrap) {
+        super(wrap);
+    }
+
+    @SuppressWarnings("deprecation")
+    public Texture(Texture other) {
+        super(0);
+        UnsafeOperations.setSFMLObjectPointer(this, other.nativeCopy());
+        UnsafeOperations.manageSFMLObject(this, true);
+    }
+
+    @Override
+    protected native long nativeCreate();
+
+    private native long nativeCopy();
+
+    /**
+     * Generates an empty texture with the given dimensions.
+     *
+     * @param width  The texture's width.
+     * @param height The texture's height.
+     * @return <tt>true</tt> if the texture was successfully created, <tt>false</tt> otherwise.
+     */
+    public native boolean create(int width, int height);
+
+    private native boolean loadFromMemory(byte[] memory, IntRect area);
+
+    /**
+     * Fully loads all available bytes from an {@link InputStream} and attempts to load the texture from it.
+     *
+     * @param in   The input stream to read from.
+     * @param area The area of the image to load into the texture.
+     * @return <tt>true</tt> if the texture was successfully loaded, <tt>false</tt> otherwise.
+     * @throws IOException In case an I/O error occurs.
+     */
+    public boolean loadFromStream(InputStream in, IntRect area) throws IOException {
+        if (area == null)
+            throw new IllegalArgumentException("area must not be null.");
+
+        int n = in.available();
+        byte[] b = new byte[n];
+
+        n = in.read(b);
+        if (n != b.length)
+            throw new IOException("Read error, expected " + b.length + ", got " + n + ".");
+
+        return loadFromMemory(b, area);
+    }
+
+    /**
+     * Fully loads all available bytes from an {@link InputStream} and attempts to load the texture from it.
+     *
+     * @param in The input stream to read from.
+     * @return <tt>true</tt> if the texture was successfully loaded, <tt>false</tt> otherwise.
+     * @throws IOException In case an I/O error occurs.
+     */
+    public boolean loadFromStream(InputStream in) throws IOException {
+        return loadFromStream(in, new IntRect());
+    }
+
+    /**
+     * Attempts to load the texture from a file.
+     *
+     * @param file The file to load the texture from.
+     * @param area The area of the image to load into the texture.
+     * @return <tt>true</tt> if the texture was successfully loaded, <tt>false</tt> otherwise.
+     * @throws IOException In case an I/O error occurs.
+     */
+    public boolean loadFromFile(File file, IntRect area) throws IOException {
+        if (area == null)
+            throw new IllegalArgumentException("area must not be null.");
+
+        InputStream in = new FileInputStream(file);
+
+        boolean result = false;
+        IOException ioException = null;
+
+        try {
+            result = loadFromStream(in, area);
+        } catch (IOException ex) {
+            ioException = ex;
+        } finally {
+            in.close();
+        }
+
+        if (ioException != null)
+            throw ioException;
+
+        return result;
+    }
+
+    /**
+     * Attempts to load the texture from a file.
+     *
+     * @param file The file to load the texture from.
+     * @return <tt>true</tt> if the texture was successfully loaded, <tt>false</tt> otherwise.
+     * @throws IOException In case an I/O error occurs.
+     */
+    public boolean loadFromFile(File file) throws IOException {
+        return loadFromFile(file, new IntRect());
+    }
+
+    //TODO public native boolean loadFromImage(Image image, IntRect area);
+
+    /**
+     * Gets the width of the texture.
+     *
+     * @return The width of the texture.
+     */
+    public native int getWidth();
+
+    /**
+     * Gets the height of the texture.
+     *
+     * @return The width of the texture.
+     */
+    public native int getHeight();
+
+    //TODO public native Image copyToImage();
+
+    //TODO public native void update(Image image, int x, int y);
+
+    private native void nativeUpdate(Window window, int x, int y);
+
+    /**
+     * Updates a part of the texture from the contents of a window.
+     *
+     * @param window The window to update from.
+     * @param x      The X offset inside the texture.
+     * @param y      The Y offset inside the texture.
+     */
+    public void update(Window window, int x, int y) {
+        if (window == null)
+            throw new IllegalArgumentException("window must not be null.");
+
+        nativeUpdate(window, x, y);
+    }
+
+    /**
+     * Updates the texture from the contents of a window.
+     *
+     * @param window The window to update from.
+     */
+    public void update(Window window) {
+        update(window, 0, 0);
+    }
+
+    private native void nativeBind(CoordinateType type);
+
+    /**
+     * Activates the texture for rendering.
+     *
+     * @param coordinateType The coordinate type to use.
+     */
+    public void bind(CoordinateType coordinateType) {
+        if (coordinateType == null)
+            throw new IllegalArgumentException("coordinateType must not be null.");
+
+        nativeBind(coordinateType);
+    }
+
+    /**
+     * Activates the texture for rendering.
+     */
+    public void bind() {
+        bind(CoordinateType.NORMALIZED);
+    }
+
+    /**
+     * Enables or disables the smooth filter.
+     * <p/>
+     * The smooth filter is disabled by default.
+     *
+     * @param smooth <tt>true</tt> to enable, <tt>false</tt> to disable.
+     */
+    public native void setSmooth(boolean smooth);
+
+    /**
+     * Checks whether the smooth filter is enabled.
+     *
+     * @return <tt>true</tt> if enabled, <tt>false</tt> if disabled.
+     */
+    public native boolean isSmooth();
+
+    /**
+     * Enables or disables texture repeating.
+     * <p/>
+     * Texture repeating is disabled by default.
+     *
+     * @param repeated <tt>true</tt> to enable, <tt>false</tt> to disable.
+     */
+    public native void setRepeated(boolean repeated);
+
+    /**
+     * Checks whether texture repeating is enabled.
+     *
+     * @return <tt>true</tt> if enabled, <tt>false</tt> if disabled.
+     */
+    public native boolean isRepeated();
+}
