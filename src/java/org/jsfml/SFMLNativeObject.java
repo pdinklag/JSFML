0b6435b8b632b69fae73c2694fe57052a6f1a692
pdinklag <pdinklag@googlemail.com>
2012-01-15 17:45:01 +0100

    Added debug output for manage/unmanage
    Signed-off-by: pdinklag <pdinklag@googlemail.com>

--- src/java/org/jsfml/SFMLNativeObject.java
+++ src/java/org/jsfml/SFMLNativeObject.java
@@ -71,9 +71,11 @@ public abstract class SFMLNativeObject {
         if (wasWrapped && !wrapped) {
             numWrapped--;
             numManaged++;
+            System.out.println("SFMLNativeObject [MANAGE]   " + this + " - ptr=0x" + Long.toHexString(ptr).toUpperCase() + " - numManaged=" + numManaged + " - numWrapped=" + numWrapped);
         } else if (!wasWrapped && wrapped) {
             numWrapped++;
             numManaged--;
+            System.out.println("SFMLNativeObject [UNMANAGE]   " + this + " - ptr=0x" + Long.toHexString(ptr).toUpperCase() + " - numManaged=" + numManaged + " - numWrapped=" + numWrapped);
         }
     }
 
