3e538d172bf4db0df1ccf479eb644195c60af039
pdinklag <pdinklag@googlemail.com>
2012-01-15 15:48:02 +0100

    Added Shader class
    Signed-off-by: pdinklag <pdinklag@googlemail.com>

---
+++ src/cpp/JNI/org_jsfml_graphics_Shader.cpp
@@ -0,0 +1,132 @@
+#include <JSFML/JNI/org_jsfml_graphics_Shader.h>
+
+#include <JSFML/Intercom/Color.hpp>
+#include <JSFML/Intercom/JavaEnum.hpp>
+#include <JSFML/Intercom/JavaString.hpp>
+#include <JSFML/Intercom/NativeObject.hpp>
+#include <JSFML/Intercom/Transform.hpp>
+
+#include <SFML/Graphics/Shader.hpp>
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    nativeCreate
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_org_jsfml_graphics_Shader_nativeCreate (JNIEnv *env, jobject obj) {
+    return (jlong)new sf::Shader();
+}
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    nativeLoadFromSource
+ * Signature: (Ljava/lang/String;Lorg/jsfml/graphics/Shader$Type;)Z
+ */
+JNIEXPORT jboolean JNICALL Java_org_jsfml_graphics_Shader_nativeLoadFromSource__Ljava_lang_String_2Lorg_jsfml_graphics_Shader_00024Type_2
+    (JNIEnv *env, jobject obj, jstring source, jobject type) {
+
+    return THIS(sf::Shader)->LoadFromMemory(
+        std::string(JavaString::getUTF8(env, source)),
+        (sf::Shader::Type)JavaEnum::ordinal(env, type));
+}
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    nativeLoadFromSource
+ * Signature: (Ljava/lang/String;Ljava/lang/String;)Z
+ */
+JNIEXPORT jboolean JNICALL Java_org_jsfml_graphics_Shader_nativeLoadFromSource__Ljava_lang_String_2Ljava_lang_String_2
+    (JNIEnv *env, jobject obj, jstring vertSource, jstring fragSource) {
+
+    return THIS(sf::Shader)->LoadFromMemory(
+        std::string(JavaString::getUTF8(env, vertSource)),
+        std::string(JavaString::getUTF8(env, fragSource)));
+}
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    nativeSetParameter
+ * Signature: (Ljava/lang/String;F)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Shader_nativeSetParameter__Ljava_lang_String_2F
+    (JNIEnv *env, jobject obj, jstring name, jfloat x) {
+
+    THIS(sf::Shader)->SetParameter(std::string(JavaString::getUTF8(env, name)), x);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    nativeSetParameter
+ * Signature: (Ljava/lang/String;FF)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Shader_nativeSetParameter__Ljava_lang_String_2FF
+    (JNIEnv *env, jobject obj, jstring name, jfloat x, jfloat y) {
+
+    THIS(sf::Shader)->SetParameter(std::string(JavaString::getUTF8(env, name)), x, y);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    nativeSetParameter
+ * Signature: (Ljava/lang/String;FFF)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Shader_nativeSetParameter__Ljava_lang_String_2FFF
+    (JNIEnv *env, jobject obj, jstring name, jfloat x, jfloat y, jfloat z) {
+
+    THIS(sf::Shader)->SetParameter(std::string(JavaString::getUTF8(env, name)), x, y, z);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    nativeSetParameter
+ * Signature: (Ljava/lang/String;FFFF)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Shader_nativeSetParameter__Ljava_lang_String_2FFFF
+    (JNIEnv *env, jobject obj, jstring name, jfloat x, jfloat y, jfloat z, jfloat w) {
+
+    THIS(sf::Shader)->SetParameter(std::string(JavaString::getUTF8(env, name)), x, y, z, w);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    nativeSetParameter
+ * Signature: (Ljava/lang/String;Lorg/jsfml/graphics/Transform;)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Shader_nativeSetParameter__Ljava_lang_String_2Lorg_jsfml_graphics_Transform_2
+    (JNIEnv *env, jobject obj, jstring name, jobject xform) {
+
+    THIS(sf::Shader)->SetParameter(
+        std::string(JavaString::getUTF8(env, name)),
+        JSFML::Transform::ToSFML(env, xform));
+}
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    nativeSetParameter
+ * Signature: (Ljava/lang/String;Lorg/jsfml/graphics/Texture;)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Shader_nativeSetParameter__Ljava_lang_String_2Lorg_jsfml_graphics_Texture_2
+    (JNIEnv *env, jobject obj, jstring name, jobject texture) {
+
+    THIS(sf::Shader)->SetParameter(
+        std::string(JavaString::getUTF8(env, name)),
+        *JSFML::NativeObject::GetPointer<sf::Texture>(env, texture));
+}
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    bind
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Shader_bind (JNIEnv *env, jobject obj) {
+    THIS(sf::Shader)->Bind();
+}
+
+/*
+ * Class:     org_jsfml_graphics_Shader
+ * Method:    unbind
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Shader_unbind (JNIEnv *env, jobject obj) {
+    THIS(sf::Shader)->Unbind();
+}
---
+++ src/java/org/jsfml/graphics/Shader.java
@@ -0,0 +1,341 @@
+package org.jsfml.graphics;
+
+import org.jsfml.SFMLNativeObject;
+import org.jsfml.system.Vector2f;
+import org.jsfml.system.Vector3f;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Shader program consisting of a vertex and a fragment shader.
+ */
+public class Shader extends SFMLNativeObject {
+    /**
+     * Shader type enumeration.
+     */
+    public enum Type {
+        /**
+         * Type for vertex shaders.
+         */
+        VERTEX,
+
+        /**
+         * Type for fragment a.k.a. pixel shaders.
+         */
+        FRAGMENT
+    }
+
+    /**
+     * Creates a new shader.
+     */
+    public Shader() {
+        super();
+    }
+
+    @Override
+    protected native long nativeCreate();
+
+    private native boolean nativeLoadFromSource(String source, Type shaderType);
+
+    private native boolean nativeLoadFromSource(String vertSource, String fragSource);
+
+    /**
+     * Attempts to load a shader from a source code.
+     *
+     * @param source     The source code.
+     * @param shaderType The shader type.
+     * @return <tt>true</tt> if the shader was successfully loaded, <tt>false</tt> otherwise.
+     */
+    public boolean loadFromSource(String source, Type shaderType) {
+        if (source == null)
+            throw new IllegalArgumentException("source must not be null.");
+
+        if (shaderType == null)
+            throw new IllegalArgumentException("shaderType must not be null.");
+
+        return nativeLoadFromSource(source, shaderType);
+    }
+
+    /**
+     * Attempts to load a shader from a source code.
+     *
+     * @param vertexShaderSource   The vertex shader's source code.
+     * @param fragmentShaderSource The fragment shader's source code.
+     * @return <tt>true</tt> if the shader was successfully loaded, <tt>false</tt> otherwise.
+     */
+    public boolean loadFromSource(String vertexShaderSource, String fragmentShaderSource) {
+        if (vertexShaderSource == null)
+            throw new IllegalArgumentException("vertexShaderSource must not be null.");
+
+        if (fragmentShaderSource == null)
+            throw new IllegalArgumentException("fragmentShaderSource must not be null.");
+
+        return nativeLoadFromSource(vertexShaderSource, fragmentShaderSource);
+    }
+
+    /**
+     * Fully loads all available bytes from an {@link InputStream} and attempts to load the shader from it.
+     *
+     * @param in         The input stream to read from.
+     * @param shaderType The shader type.
+     * @return <tt>true</tt> if the shader was successfully loaded, <tt>false</tt> otherwise.
+     * @throws IOException In case an I/O error occurs.
+     */
+    public boolean loadFromStream(InputStream in, Type shaderType) throws IOException {
+        if (shaderType == null)
+            throw new IllegalArgumentException("shaderType must not be null.");
+
+        int n = in.available();
+        byte[] b = new byte[n];
+
+        n = in.read(b);
+        if (n != b.length)
+            throw new IOException("Read error, expected " + b.length + ", got " + n + ".");
+
+        return loadFromSource(new String(b), shaderType);
+    }
+
+    /**
+     * Fully loads all available bytes from an {@link InputStream} and attempts to load the shader from it.
+     *
+     * @param vertexShaderIn   The input stream to read the vertex shader from.
+     * @param fragmentShaderIn The input stream to read the fragment shader from.
+     * @return <tt>true</tt> if the shader was successfully loaded, <tt>false</tt> otherwise.
+     * @throws IOException In case an I/O error occurs.
+     */
+    public boolean loadFromStream(InputStream vertexShaderIn, InputStream fragmentShaderIn) throws IOException {
+        String vertSource, fragSource;
+
+        {
+            int n = vertexShaderIn.available();
+            byte[] b = new byte[n];
+
+            n = vertexShaderIn.read(b);
+            if (n != b.length)
+                throw new IOException("Read error, expected " + b.length + ", got " + n + ".");
+
+            vertSource = new String(b);
+        }
+
+        {
+            int n = fragmentShaderIn.available();
+            byte[] b = new byte[n];
+
+            n = fragmentShaderIn.read(b);
+            if (n != b.length)
+                throw new IOException("Read error, expected " + b.length + ", got " + n + ".");
+
+            fragSource = new String(b);
+        }
+
+        return loadFromSource(vertSource, fragSource);
+    }
+
+    /**
+     * Attempts to load the shader from a file.
+     *
+     * @param file       The file to load.
+     * @param shaderType The shader type.
+     * @return <tt>true</tt> if the shader was successfully loaded, <tt>false</tt> otherwise.
+     * @throws IOException In case an I/O error occurs.
+     */
+    public boolean loadFromFile(File file, Type shaderType) throws IOException {
+        if (shaderType == null)
+            throw new IllegalArgumentException("shaderType must not be null.");
+
+        InputStream in = new FileInputStream(file);
+        boolean result = loadFromStream(in, shaderType);
+        in.close();
+
+        return result;
+    }
+
+    /**
+     * Attempts to load the shader from files.
+     *
+     * @param vertexShaderFile   The file to read the vertex shader from.
+     * @param fragmentShaderFile The file to read the fragment shader from.
+     * @return <tt>true</tt> if the shader was successfully loaded, <tt>false</tt> otherwise.
+     * @throws IOException In case an I/O error occurs.
+     */
+    public boolean loadFromFile(File vertexShaderFile, File fragmentShaderFile) throws IOException {
+        IOException ioException = null;
+
+        InputStream vertIn = null;
+        InputStream fragIn = null;
+
+        boolean result = false;
+
+        try {
+            vertIn = new FileInputStream(vertexShaderFile);
+            fragIn = new FileInputStream(fragmentShaderFile);
+
+            result = loadFromStream(vertIn, fragIn);
+
+        } catch (IOException ex) {
+            ioException = ex;
+        } finally {
+            try {
+                if (vertIn != null)
+                    vertIn.close();
+            } catch (IOException ex) {
+                //
+            }
+
+            if (fragIn != null)
+                fragIn.close();
+        }
+
+        if (ioException != null)
+            throw ioException;
+
+        return result;
+    }
+
+    private native void nativeSetParameter(String name, float x);
+
+    /**
+     * Sets a float parameter (<tt>float</tt>) value in the shader.
+     *
+     * @param name The parameter's name.
+     * @param x    The parameter's value.
+     */
+    public void setParameter(String name, float x) {
+        if (name == null)
+            throw new IllegalArgumentException("name must not be null.");
+
+        nativeSetParameter(name, x);
+    }
+
+    private native void nativeSetParameter(String name, float x, float y);
+
+    /**
+     * Sets a 2-component-float (<tt>vec2</tt>) parameter value in the shader.
+     *
+     * @param name The parameter's name.
+     * @param x    The parameter's value.
+     * @param y    The parameter's value.
+     */
+    public void setParameter(String name, float x, float y) {
+        if (name == null)
+            throw new IllegalArgumentException("name must not be null.");
+
+        nativeSetParameter(name, x, y);
+    }
+
+    /**
+     * Sets a 2-component-float (<tt>vec2</tt>) parameter value in the shader.
+     *
+     * @param name The parameter's name.
+     * @param v    The parameter's value.
+     */
+    public void setParameter(String name, Vector2f v) {
+        setParameter(name, v.x, v.y);
+    }
+
+    private native void nativeSetParameter(String name, float x, float y, float z);
+
+    /**
+     * Sets a 3-component-float (<tt>vec3</tt>) parameter value in the shader.
+     *
+     * @param name The parameter's name.
+     * @param x    The parameter's value.
+     * @param y    The parameter's value.
+     * @param z    The parameter's value.
+     */
+    public void setParameter(String name, float x, float y, float z) {
+        if (name == null)
+            throw new IllegalArgumentException("name must not be null.");
+
+        nativeSetParameter(name, x, y, z);
+    }
+
+    /**
+     * Sets a 3-component-float (<tt>vec3</tt>) parameter value in the shader.
+     *
+     * @param name The parameter's name.
+     * @param v    The parameter's value.
+     */
+    public void setParameter(String name, Vector3f v) {
+        setParameter(name, v.x, v.y, v.z);
+    }
+
+    private native void nativeSetParameter(String name, float x, float y, float z, float w);
+
+    /**
+     * Sets a 4-component-float (<tt>vec4</tt>) parameter value in the shader.
+     *
+     * @param name The parameter's name.
+     * @param x    The parameter's value.
+     * @param y    The parameter's value.
+     * @param z    The parameter's value.
+     * @param w    The parameter's value.
+     */
+    public void setParameter(String name, float x, float y, float z, float w) {
+        if (name == null)
+            throw new IllegalArgumentException("name must not be null.");
+
+        nativeSetParameter(name, x, y, z, w);
+    }
+
+    /**
+     * Sets a color (vec4) parameter value in the shader.
+     *
+     * @param name  The parameter's name.
+     * @param color The parameter's value.
+     */
+    public void setParameter(String name, Color color) {
+        setParameter(name,
+                (float) color.getR() / 255.0f,
+                (float) color.getG() / 255.0f,
+                (float) color.getB() / 255.0f,
+                (float) color.getA() / 255.0f);
+    }
+
+    private native void nativeSetParameter(String name, Transform xform);
+
+    /**
+     * Sets a matrix (mat4) parameter value in the shader.
+     * @param name  The parameter's name.
+     * @param xform The parameter's value.
+     */
+    public void setParameter(String name, Transform xform) {
+        if (name == null)
+            throw new IllegalArgumentException("name must not be null.");
+
+        if (xform == null)
+            throw new IllegalArgumentException("xform must not be null.");
+
+        nativeSetParameter(name, xform);
+    }
+
+    private native void nativeSetParameter(String name, Texture texture);
+
+    /**
+     * Sets a texture (sampler2D) parameter value in the shader.
+     * @param name  The parameter's name.
+     * @param texture The parameter's value.
+     */
+    public void setParameter(String name, Texture texture) {
+        if (name == null)
+            throw new IllegalArgumentException("name must not be null.");
+
+        if (texture == null)
+            throw new IllegalArgumentException("texture must not be null.");
+
+        nativeSetParameter(name, texture);
+    }
+
+    /**
+     * Activates the shader for rendering.
+     */
+    public native void bind();
+
+    /**
+     * Deactivates the shader for rendering.
+     */
+    public native void unbind();
+}
