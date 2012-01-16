d70d2aed2d1ceec9a361c5b0c5284322a65c47db
pdinklag <pdinklag@googlemail.com>
2012-01-16 21:34:23 +0100

    Added RenderTexture class
    Signed-off-by: pdinklag <pdinklag@googlemail.com>

---
+++ src/cpp/JNI/org_jsfml_graphics_RenderTexture.cpp
@@ -0,0 +1,218 @@
+#include <JSFML/JNI/org_jsfml_graphics_RenderTexture.h>
+
+#include <JSFML/Intercom/Color.hpp>
+#include <JSFML/Intercom/IntRect.hpp>
+#include <JSFML/Intercom/NativeObject.hpp>
+#include <JSFML/Intercom/JavaEnum.hpp>
+#include <JSFML/Intercom/RenderStates.hpp>
+#include <JSFML/Intercom/Vector2f.hpp>
+#include <JSFML/Intercom/Vertex.hpp>
+
+#include <SFML/Graphics/RenderTexture.hpp>
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    nativeCreate
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_org_jsfml_graphics_RenderTexture_nativeCreate (JNIEnv *env, jobject obj) {
+    return (jlong)new sf::RenderTexture();
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    create
+ * Signature: (IIZ)Z
+ */
+JNIEXPORT jboolean JNICALL Java_org_jsfml_graphics_RenderTexture_create
+    (JNIEnv *env, jobject obj, jint width, jint height, jboolean depthBuffer) {
+
+    return THIS(sf::RenderTexture)->Create(width, height, depthBuffer);
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    setSmooth
+ * Signature: (Z)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_RenderTexture_setSmooth (JNIEnv *env, jobject obj, jboolean b) {
+    THIS(sf::RenderTexture)->SetSmooth(b);
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    isSmooth
+ * Signature: ()Z
+ */
+JNIEXPORT jboolean JNICALL Java_org_jsfml_graphics_RenderTexture_isSmooth (JNIEnv *env, jobject obj) {
+    return THIS(sf::RenderTexture)->IsSmooth();
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    setActive
+ * Signature: (Z)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_RenderTexture_setActive (JNIEnv *env, jobject obj, jboolean b) {
+    THIS(sf::RenderTexture)->SetActive(b);
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    display
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_RenderTexture_display (JNIEnv *env, jobject obj) {
+    THIS(sf::RenderTexture)->Display();
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    nativeGetTexture
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_org_jsfml_graphics_RenderTexture_nativeGetTexture (JNIEnv *env, jobject obj) {
+    return (jlong)&THIS(sf::RenderTexture)->GetTexture();
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    getWidth
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_org_jsfml_graphics_RenderTexture_getWidth (JNIEnv *env, jobject obj) {
+    return THIS(sf::RenderTexture)->GetWidth();
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    getHeight
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_org_jsfml_graphics_RenderTexture_getHeight (JNIEnv *env, jobject obj) {
+    return THIS(sf::RenderTexture)->GetHeight();
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    nativeClear
+ * Signature: (Lorg/jsfml/graphics/Color;)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_RenderTexture_nativeClear (JNIEnv *env, jobject obj, jobject color) {
+    THIS(sf::RenderTexture)->Clear(JSFML::Color::ToSFML(env, color));
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    nativeSetView
+ * Signature: (Lorg/jsfml/graphics/View;)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_RenderTexture_nativeSetView (JNIEnv *env, jobject obj, jobject view) {
+    THIS(sf::RenderTexture)->SetView(*JSFML::NativeObject::GetPointer<sf::View>(env, view));
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    nativeGetDefaultView
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_org_jsfml_graphics_RenderTexture_nativeGetDefaultView (JNIEnv *env, jobject obj) {
+    return (jlong)&THIS(sf::RenderTexture)->GetDefaultView();
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    nativeGetViewport
+ * Signature: (Lorg/jsfml/graphics/View;)Lorg/jsfml/graphics/IntRect;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_RenderTexture_nativeGetViewport (JNIEnv *env, jobject obj, jobject view) {
+    return JSFML::IntRect::FromSFML(
+        env,
+        THIS(sf::RenderTexture)->GetViewport(*JSFML::NativeObject::GetPointer<sf::View>(env, view)));
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    convertCoords
+ * Signature: (FF)Lorg/jsfml/system/Vector2f;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_RenderTexture_convertCoords
+    (JNIEnv * env, jobject obj, jfloat x, jfloat y) {
+
+    return JSFML::Vector2f::FromSFML(env, THIS(sf::RenderTexture)->ConvertCoords(x, y));
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    nativeConvertCoords
+ * Signature: (FFLorg/jsfml/graphics/View;)Lorg/jsfml/system/Vector2f;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_RenderTexture_nativeConvertCoords
+    (JNIEnv *env, jobject obj, jfloat x, jfloat y, jobject view) {
+
+    return JSFML::Vector2f::FromSFML(env,
+        THIS(sf::RenderTexture)->ConvertCoords(x, y, *JSFML::NativeObject::GetPointer<sf::View>(env, view)));
+ }
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    nativeDraw
+ * Signature: (Lorg/jsfml/graphics/Drawable;Lorg/jsfml/graphics/RenderStates;)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_RenderTexture_nativeDraw__Lorg_jsfml_graphics_Drawable_2Lorg_jsfml_graphics_RenderStates_2
+    (JNIEnv *env, jobject obj, jobject drawable, jobject renderStates) {
+
+    THIS(sf::RenderTexture)->Draw(
+        *JSFML::NativeObject::GetPointer<sf::Drawable>(env, drawable),
+        JSFML::RenderStates::ToSFML(env, renderStates));
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    nativeDraw
+ * Signature: ([Lorg/jsfml/graphics/Vertex;Lorg/jsfml/graphics/PrimitiveType;Lorg/jsfml/graphics/RenderStates;)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_RenderTexture_nativeDraw___3Lorg_jsfml_graphics_Vertex_2Lorg_jsfml_graphics_PrimitiveType_2Lorg_jsfml_graphics_RenderStates_2
+    (JNIEnv *env, jobject obj, jobjectArray vertices, jobject type, jobject renderStates) {
+
+    jint num = env->GetArrayLength(vertices);
+    if(num > 0) {
+        sf::Vertex sfmlVertices[env->GetArrayLength(vertices)];
+
+        for(jint i = 0; i < num; i++)
+            sfmlVertices[i] = JSFML::Vertex::ToSFML(env, env->GetObjectArrayElement(vertices, i));
+
+        THIS(sf::RenderTexture)->Draw(
+            sfmlVertices,
+            num,
+            (sf::PrimitiveType)JavaEnum::ordinal(env, type),
+            JSFML::RenderStates::ToSFML(env, renderStates));
+    }
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    pushGLStates
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_RenderTexture_pushGLStates (JNIEnv *env, jobject obj) {
+    THIS(sf::RenderTexture)->PushGLStates();
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    popGLStates
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_RenderTexture_popGLStates (JNIEnv *env, jobject obj) {
+    THIS(sf::RenderTexture)->PopGLStates();
+}
+
+/*
+ * Class:     org_jsfml_graphics_RenderTexture
+ * Method:    resetGLStates
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_RenderTexture_resetGLStates (JNIEnv *env, jobject obj) {
+    THIS(sf::RenderTexture)->ResetGLStates();
+}
---
+++ src/java/org/jsfml/graphics/RenderTexture.java
@@ -0,0 +1,193 @@
+package org.jsfml.graphics;
+
+import org.jsfml.SFMLNativeObject;
+import org.jsfml.system.Vector2f;
+
+/**
+ * Target for off-screen 2D rendering into a texture.
+ */
+public class RenderTexture extends SFMLNativeObject implements RenderTarget {
+    private View view, defaultView;
+    private ImmutableTexture texture;
+
+    /**
+     * Creates a new render texture.
+     */
+    public RenderTexture() {
+        super();
+
+        defaultView = new View(nativeGetDefaultView());
+        view = defaultView;
+        texture = new ImmutableTexture(nativeGetTexture());
+    }
+
+    @Override
+    protected native long nativeCreate();
+
+    /**
+     * Sets up the render texture.
+     *
+     * @param width       The texture's width.
+     * @param height      The texture's height.
+     * @param depthBuffer <tt>true</tt> to generate a depth buffer, <tt>false</tt> otherwise.
+     * @return <tt>true</tt> if the render texture was set up successfully, <tt>false</tt> otherwise.
+     */
+    public native boolean create(int width, int height, boolean depthBuffer);
+
+    /**
+     * Enables or disables textures smoothing.
+     *
+     * @param smooth <tt>true</tt> to enable, <tt>false</tt> to disable.
+     */
+    public native void setSmooth(boolean smooth);
+
+    /**
+     * Checks whether texture smoothing is enabled.
+     *
+     * @return <tt>true</tt> if enabled, <tt>false</tt> if not.
+     */
+    public native boolean isSmooth();
+
+    /**
+     * Activates or deactivates the render texture for rendering.
+     *
+     * @param active <tt>true</tt> to activate, <tt>false</tt> to deactivate.
+     */
+    public native void setActive(boolean active);
+
+    /**
+     * Updates the contents of the target texture.
+     */
+    public native void display();
+
+    private native long nativeGetTexture();
+
+    /**
+     * Gets the target texture.
+     *
+     * @return The target texture.
+     */
+    public Texture getTexture() {
+        return texture;
+    }
+
+    @Override
+    public native int getWidth();
+
+    @Override
+    public native int getHeight();
+
+    private native void nativeClear(Color color);
+
+    @Override
+    public void clear(Color color) {
+        if (color == null)
+            throw new IllegalArgumentException("color must not be null.");
+
+        nativeClear(color);
+    }
+
+    /**
+     * Clears the target with black.
+     */
+    public void clear() {
+        nativeClear(Color.BLACK);
+    }
+
+    private native void nativeSetView(View view);
+
+    @Override
+    public void setView(View view) {
+        if (view == null)
+            throw new IllegalArgumentException("view must not be null.");
+
+        this.view = view;
+        nativeSetView(view);
+    }
+
+    @Override
+    public View getView() {
+        return view;
+    }
+
+    private native long nativeGetDefaultView();
+
+    @Override
+    public View getDefaultView() {
+        return defaultView;
+    }
+
+    private native IntRect nativeGetViewport(View view);
+
+    @Override
+    public IntRect getViewport(View view) {
+        if (view == null)
+            throw new IllegalArgumentException("view must not be null.");
+
+        return nativeGetViewport(view);
+    }
+
+    @Override
+    public native Vector2f convertCoords(float x, float y);
+
+    private native Vector2f nativeConvertCoords(float x, float y, View view);
+
+    @Override
+    public Vector2f convertCoords(float x, float y, View view) {
+        if (view == null)
+            throw new IllegalArgumentException("view must not be null.");
+
+        return nativeConvertCoords(x, y, view);
+    }
+
+    @Override
+    public void draw(Drawable drawable) {
+        draw(drawable, new RenderStates());
+    }
+
+    private native void nativeDraw(Drawable drawable, RenderStates states);
+
+    @Override
+    public void draw(Drawable drawable, RenderStates renderStates) {
+        if (drawable == null)
+            throw new IllegalArgumentException("drawable must not be null.");
+
+        if (!(drawable instanceof SFMLNativeObject))
+            throw new IllegalArgumentException("drawable must be a native SFML object.");
+
+        if (renderStates == null)
+            throw new IllegalArgumentException("renderStates must not be null.");
+
+        nativeDraw(drawable, renderStates);
+    }
+
+    @Override
+    public void draw(Vertex[] vertices, PrimitiveType type) {
+        draw(vertices, type, new RenderStates());
+    }
+
+    private native void nativeDraw(Vertex[] vertices, PrimitiveType type, RenderStates states);
+
+    @Override
+    public void draw(Vertex[] vertices, PrimitiveType type, RenderStates states) {
+        if (vertices == null)
+            throw new IllegalArgumentException("vertices must not be null.");
+
+        if (type == null)
+            throw new IllegalArgumentException("type must not be null.");
+
+        if (states == null)
+            throw new IllegalArgumentException("states must not be null.");
+
+        nativeDraw(vertices, type, states);
+    }
+
+    @Override
+    public native void pushGLStates();
+
+    @Override
+    public native void popGLStates();
+
+    @Override
+    public native void resetGLStates();
+}
