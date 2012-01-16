0ea9b626e09676fe7797ce260906b28e2ef5e1cf
pdinklag <pdinklag@googlemail.com>
2012-01-16 21:04:38 +0100

    Added Text class
    Signed-off-by: pdinklag <pdinklag@googlemail.com>

---
+++ src/cpp/JNI/org_jsfml_graphics_Text.cpp
@@ -0,0 +1,234 @@
+#include <JSFML/JNI/org_jsfml_graphics_Text.h>
+
+#include <JSFML/Intercom/Color.hpp>
+#include <JSFML/Intercom/FloatRect.hpp>
+#include <JSFML/Intercom/JavaString.hpp>
+#include <JSFML/Intercom/NativeObject.hpp>
+#include <JSFML/Intercom/Transform.hpp>
+#include <JSFML/Intercom/Vector2f.hpp>
+
+#include <SFML/Graphics/Text.hpp>
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    nativeCreate
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_org_jsfml_graphics_Text_nativeCreate (JNIEnv *env, jobject obj) {
+    return (jlong)new sf::Text();
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    nativeSetString
+ * Signature: (Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_nativeSetString
+    (JNIEnv *env, jobject obj, jstring str) {
+
+    THIS(sf::Text)->SetString(sf::String(JavaString::getUnicode(env, str)));
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    nativeSetFont
+ * Signature: (Lorg/jsfml/graphics/Font;)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_nativeSetFont
+    (JNIEnv *env, jobject obj, jobject font) {
+
+    THIS(sf::Text)->SetFont(*JSFML::NativeObject::GetPointer<sf::Font>(env, font));
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    setCharacterSize
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_setCharacterSize
+    (JNIEnv *env, jobject obj, jint size) {
+
+    THIS(sf::Text)->SetCharacterSize(size);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    setStyle
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_setStyle
+    (JNIEnv *env, jobject obj, jint style) {
+
+    THIS(sf::Text)->SetStyle(style);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    nativeSetColor
+ * Signature: (Lorg/jsfml/graphics/Color;)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_nativeSetColor (JNIEnv *env, jobject obj, jobject color) {
+    THIS(sf::Text)->SetColor(JSFML::Color::ToSFML(env, color));
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getCharacterSize
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_org_jsfml_graphics_Text_getCharacterSize (JNIEnv *env, jobject obj) {
+    return THIS(sf::Text)->GetCharacterSize();
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getStyle
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_org_jsfml_graphics_Text_getStyle (JNIEnv *env, jobject obj) {
+    return THIS(sf::Text)->GetStyle();
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getColor
+ * Signature: ()Lorg/jsfml/graphics/Color;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_Text_getColor (JNIEnv *env, jobject obj) {
+    return JSFML::Color::FromSFML(env, THIS(sf::Text)->GetColor());
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getLocalBounds
+ * Signature: ()Lorg/jsfml/graphics/FloatRect;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_Text_getLocalBounds (JNIEnv *env, jobject obj) {
+    return JSFML::FloatRect::FromSFML(env, THIS(sf::Text)->GetLocalBounds());
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getGlobalBounds
+ * Signature: ()Lorg/jsfml/graphics/FloatRect;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_Text_getGlobalBounds (JNIEnv *env, jobject obj) {
+    return JSFML::FloatRect::FromSFML(env, THIS(sf::Text)->GetGlobalBounds());
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    setPosition
+ * Signature: (FF)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_setPosition (JNIEnv *env, jobject obj, jfloat x, jfloat y) {
+    THIS(sf::Text)->SetPosition(x, y);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    setRotation
+ * Signature: (F)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_setRotation (JNIEnv *env, jobject obj, jfloat angle) {
+    THIS(sf::Text)->SetRotation(angle);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    setScale
+ * Signature: (FF)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_setScale (JNIEnv *env, jobject obj, jfloat x, jfloat y) {
+    THIS(sf::Text)->SetScale(x, y);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    setOrigin
+ * Signature: (FF)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_setOrigin (JNIEnv *env, jobject obj, jfloat x, jfloat y) {
+    THIS(sf::Text)->SetOrigin(x, y);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getPosition
+ * Signature: ()Lorg/jsfml/system/Vector2f;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_Text_getPosition (JNIEnv *env, jobject obj) {
+    return JSFML::Vector2f::FromSFML(env, THIS(sf::Text)->GetPosition());
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getRotation
+ * Signature: ()F
+ */
+JNIEXPORT jfloat JNICALL Java_org_jsfml_graphics_Text_getRotation (JNIEnv *env, jobject obj) {
+    return THIS(sf::Text)->GetRotation();
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getScale
+ * Signature: ()Lorg/jsfml/system/Vector2f;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_Text_getScale (JNIEnv *env, jobject obj) {
+    return JSFML::Vector2f::FromSFML(env, THIS(sf::Text)->GetScale());
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getOrigin
+ * Signature: ()Lorg/jsfml/system/Vector2f;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_Text_getOrigin (JNIEnv *env, jobject obj) {
+    return JSFML::Vector2f::FromSFML(env, THIS(sf::Text)->GetOrigin());
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    move
+ * Signature: (FF)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_move (JNIEnv *env, jobject obj, jfloat x, jfloat y) {
+    THIS(sf::Text)->Move(x, y);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    rotate
+ * Signature: (F)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_rotate (JNIEnv *env, jobject obj, jfloat angle) {
+   THIS(sf::Text)->Rotate(angle);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    scale
+ * Signature: (FF)V
+ */
+JNIEXPORT void JNICALL Java_org_jsfml_graphics_Text_scale (JNIEnv *env, jobject obj, jfloat x, jfloat y) {
+    THIS(sf::Text)->Scale(x, y);
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getTransform
+ * Signature: ()Lorg/jsfml/graphics/Transform;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_Text_getTransform (JNIEnv *env, jobject obj) {
+    return JSFML::Transform::FromSFML(env, THIS(sf::Text)->GetTransform());
+}
+
+/*
+ * Class:     org_jsfml_graphics_Text
+ * Method:    getInverseTransform
+ * Signature: ()Lorg/jsfml/graphics/Transform;
+ */
+JNIEXPORT jobject JNICALL Java_org_jsfml_graphics_Text_getInverseTransform (JNIEnv *env, jobject obj) {
+    return JSFML::Transform::FromSFML(env, THIS(sf::Text)->GetInverseTransform());
+}
---
+++ src/java/org/jsfml/graphics/Text.java
@@ -0,0 +1,244 @@
+package org.jsfml.graphics;
+
+import org.jsfml.system.Vector2f;
+
+/**
+ * Graphical text that can be transformed and drawn to a render target.
+ */
+public class Text extends Transformable implements Drawable {
+    /**
+     * Regular drawing style.
+     */
+    public static final int REGULAR = 0;
+
+    /**
+     * Bold drawing style.
+     */
+    public static final int BOLD = 0x01;
+
+    /**
+     * Italic drawing style.
+     */
+    public static final int ITALIC = 0x02;
+
+    /**
+     * Underlined drawing style.
+     */
+    public static final int UNDERLINED = 0x04;
+
+    private Font font = Font.getDefaultFont();
+    private String string = "";
+
+    /**
+     * Creates a new empty text.
+     */
+    public Text() {
+        super();
+    }
+
+    /**
+     * Creates a new text.
+     *
+     * @param string The text string.
+     */
+    public Text(String string) {
+        this();
+        setString(string);
+    }
+
+    /**
+     * Creates a new text.
+     *
+     * @param string The text string.
+     * @param font   The font to use.
+     */
+    public Text(String string, Font font) {
+        this(string);
+        setFont(font);
+    }
+
+    /**
+     * Creates a new text.
+     *
+     * @param string        The text string.
+     * @param font          The font to use.
+     * @param characterSize The font size.
+     */
+    public Text(String string, Font font, int characterSize) {
+        this(string, font);
+        setCharacterSize(characterSize);
+    }
+
+    @Override
+    protected native long nativeCreate();
+
+    private native void nativeSetString(String string);
+
+    /**
+     * Sets the string to display.
+     *
+     * @param string The string to display.
+     */
+    public void setString(String string) {
+        if (string == null)
+            throw new IllegalArgumentException("string must not be null");
+
+        this.string = string;
+        nativeSetString(string);
+    }
+
+    private native void nativeSetFont(Font font);
+
+    /**
+     * Sets the text's font.
+     *
+     * @param font The text's font.
+     */
+    public void setFont(Font font) {
+        if (font == null)
+            throw new IllegalArgumentException("font must not be null");
+
+        this.font = font;
+        nativeSetFont(font);
+    }
+
+    /**
+     * Sets the font size for this text.
+     *
+     * @param characterSize The font size for this text.
+     */
+    public native void setCharacterSize(int characterSize);
+
+    /**
+     * Sets the font drawing style.
+     *
+     * @param style The font drawing style. This should be an ORed combination of the style flags
+     *              {@link Text#BOLD}, {@link Text#ITALIC} and {@link  Text#UNDERLINED},
+     *              or {@link Text#REGULAR} for a regular style.
+     */
+    public native void setStyle(int style);
+
+    private native void nativeSetColor(Color color);
+
+    /**
+     * Sets the font color for this text.
+     *
+     * @param color The font color for this text.
+     */
+    public void setColor(Color color) {
+        if (color == null)
+            throw new IllegalArgumentException("color must not be null");
+
+        nativeSetColor(color);
+    }
+
+    /**
+     * Gets the text's string.
+     *
+     * @return The text strng.
+     */
+    public String getString() {
+        return string;
+    }
+
+    /**
+     * Gets the text's current font.
+     *
+     * @return The text's current font.
+     */
+    public Font getFont() {
+        return font;
+    }
+
+    /**
+     * Gets the text's current font size.
+     *
+     * @return The text's current font size.
+     */
+    public native int getCharacterSize();
+
+    /**
+     * Gets the text's current font style.
+     *
+     * @return The text's current font style.
+     * @see Text#setStyle(int)
+     */
+    public native int getStyle();
+
+    /**
+     * Gets the text's current font color.
+     *
+     * @return The text's current font color.
+     */
+    public native Color getColor();
+
+    private native Vector2f nativeFindCharacterPos(int i);
+
+    /**
+     * Returns the position of a character inside the string.
+     *
+     * @param i The index of the character to return the position for.
+     * @return The position of the character at the given index.
+     */
+    public Vector2f findCharacterPos(int i) {
+        if (i < 0 || i >= string.length())
+            throw new IndexOutOfBoundsException(Integer.toString(i));
+
+        return nativeFindCharacterPos(i);
+    }
+
+    /**
+     * Gets the sprite's local bounding rectangle, <i>not</i> taking the sprite's transformation into account.
+     *
+     * @return The sprite's local bounding rectangle.
+     * @see org.jsfml.graphics.Sprite#getGlobalBounds()
+     */
+    public native FloatRect getLocalBounds();
+
+    /**
+     * Gets the sprite's global bounding rectangle, taking the sprite's transformation into account.
+     *
+     * @return The sprite's global bounding rectangle.
+     * @see org.jsfml.graphics.Sprite#getLocalBounds()
+     */
+    public native FloatRect getGlobalBounds();
+
+    @Override
+    public native void setPosition(float x, float y);
+
+    @Override
+    public native void setRotation(float angle);
+
+    @Override
+    public native void setScale(float x, float y);
+
+    @Override
+    public native void setOrigin(float x, float y);
+
+    @Override
+    public native Vector2f getPosition();
+
+    @Override
+    public native float getRotation();
+
+    @Override
+    public native Vector2f getScale();
+
+    @Override
+    public native Vector2f getOrigin();
+
+    @Override
+    public native void move(float x, float y);
+
+    @Override
+    public native void rotate(float angle);
+
+    @Override
+    public native void scale(float x, float y);
+
+    @Override
+    public native Transform getTransform();
+
+    @Override
+    public native Transform getInverseTransform();
+}
