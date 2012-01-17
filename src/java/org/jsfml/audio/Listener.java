0d9c1d17f6c49dfd46cc967fa6ff7b2e7e074388
pdinklag <pdinklag@googlemail.com>
2012-01-17 09:10:09 +0100

    TODO message in Listener
    Signed-off-by: pdinklag <pdinklag@googlemail.com>

--- src/java/org/jsfml/audio/Listener.java
+++ src/java/org/jsfml/audio/Listener.java
@@ -14,6 +14,8 @@ public class Listener {
         } catch (JSFMLException ex) {
             throw new UnsatisfiedLinkError(ex.getMessage());
         }
+
+        //TODO add audio device shutdown hook
     }
 
     /**
