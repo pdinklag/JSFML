069193e32268d81cc2908aaa976bef854f7203ad
pdinklag <pdinklag@googlemail.com>
2012-03-23 09:13:47 +0100

    The batch script to locate MSVC++ on Windows should now support versions 2005, 2008 and 2010, both Express and Professional, both 32 and 64 bit
    Signed-off-by: pdinklag <pdinklag@googlemail.com>

--- windows-find-vs.bat
+++ windows-find-vs.bat
@@ -1,4 +1,30 @@
 @echo off
 REM This batch file is used by the Windows build targets to automatically find the location of the MS Visual Studio.
-for /F "tokens=2* delims=	 " %%A in ('reg query "HKCU\Software\Microsoft\VCExpress\10.0_Config" /v ShellFolder') DO SET VS_DIR=%%B
-echo %VS_DIR%
+
+set NOTFOUND="?"
+set RESULT=%UNKNOWN%
+
+REM MSVC++ 2010 (10.0)
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Microsoft\VisualStudio\10.0\Setup\VS" "ProductDir" 2> NUL
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\10.0\Setup\VS" "ProductDir" 2> NUL
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Microsoft\VCExpress\10.0\Setup\VS" "ProductDir" 2> NUL
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Wow6432Node\Microsoft\VCExpress\10.0\Setup\VS" "ProductDir" 2> NUL
+
+REM MSVC++ 2008 (9.0)
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Microsoft\VisualStudio\9.0\Setup\VS" "ProductDir" 2> NUL
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\9.0\Setup\VS" "ProductDir" 2> NUL
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Microsoft\VCExpress\9.0\Setup\VS" "ProductDir" 2> NUL
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Wow6432Node\Microsoft\VCExpress\9.0\Setup\VS" "ProductDir" 2> NUL
+
+REM MSVC++ 2005 (8.0)
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Microsoft\VisualStudio\8.0\Setup\VS" "ProductDir" 2> NUL
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\8.0\Setup\VS" "ProductDir" 2> NUL
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Microsoft\VCExpress\8.0\Setup\VS" "ProductDir" 2> NUL
+if "%RESULT%"=="%UNKNOWN%" call:regQuery "HKLM\SOFTWARE\Wow6432Node\Microsoft\VCExpress\8.0\Setup\VS" "ProductDir" 2> NUL
+
+echo %RESULT%
+goto:eof
+
+:regQuery
+for /F "tokens=2* delims=	 " %%A in ('reg query %~1 /v %~2') do set RESULT=%%B
+goto:eof
