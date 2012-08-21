52f8bd0ea2cacd2c04c6a5d3c117d7dff5ba9b57
pdinklag <pdinklag@googlemail.com>
2012-08-21 21:03:55 +0200

    @Intercom is now for C++-invokable methods also
    Signed-off-by: pdinklag <pdinklag@googlemail.com>

--- src/java/org/jsfml/Intercom.java
+++ src/java/org/jsfml/Intercom.java
@@ -5,12 +5,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 /**
- * Annotation for intercom-critical types, fields and constructors.
+ * Annotation for intercom-critical types, fields, methods and constructors.
  * <p/>
  * This annotation is purely for information purposes and provides no actual functionality. Elements
  * annotated by this annotation must not be refactored without altering the respective C++ sources.
  */
 @Documented
-@Target({ElementType.TYPE, ElementType.FIELD, ElementType.CONSTRUCTOR})
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface Intercom {
 }
