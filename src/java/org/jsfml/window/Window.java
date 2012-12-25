f0cb51b8deafa0f560374c240eb54becc73b3200
Patrick Dinklage <pdinklag@googlemail.com>
2012-12-25 04:52:57 +0100

    Improved Window class javadoc, moved window style constants to a new interface (which Window implements).
    Signed-off-by: Patrick Dinklage <pdinklag@googlemail.com>

--- src/java/org/jsfml/window/Window.java
+++ src/java/org/jsfml/window/Window.java
@@ -8,39 +8,12 @@ import org.jsfml.window.event.Event;
 import java.util.Iterator;
 
 /**
- * Basic window that serves as an OpenGL target.
+ * A basic window that provides an OpenGL context.
+ * <p/>
+ * This class implements the {@code WindowStyle} interface for quick access
+ * to the constants provided by it.
  */
-public class Window extends SFMLNativeObject {
-	/**
-	 * Unstyled window.
-	 */
-	public static final int NONE = 0;
-
-	/**
-	 * The window will have a title bar.
-	 */
-	public static final int TITLEBAR = 0x01;
-
-	/**
-	 * The window is resizable.
-	 */
-	public static final int RESIZE = 0x02;
-
-	/**
-	 * The window has a close button.
-	 */
-	public static final int CLOSE = 0x04;
-
-	/**
-	 * The window emulates a fullscreen mode.
-	 */
-	public static final int FULLSCREEN = 0x08;
-
-	/**
-	 * The default style, a resizable and closeable window with a title bar.
-	 */
-	public static final int DEFAULT = TITLEBAR | RESIZE | CLOSE;
-
+public class Window extends SFMLNativeObject implements WindowStyle {
 	/**
 	 * The current window icon image.
 	 * <p/>
@@ -50,7 +23,7 @@ public class Window extends SFMLNativeObject {
 	private Image icon = null;
 
 	/**
-	 * Constructs a new window without creating it.
+	 * Constructs a new window without actually creating it (making it visible).
 	 *
 	 * @see Window#create(org.jsfml.window.VideoMode, String, int, org.jsfml.window.ContextSettings)
 	 */
@@ -59,6 +32,12 @@ public class Window extends SFMLNativeObject {
 		SFMLNative.ensureDisplay();
 	}
 
+	/**
+	 * Constructs a new window within the specified parent window.
+	 *
+	 * @param ptr the window handle of the parent window.
+	 * @deprecated Use of this method may cause undefined behaviour and is not supported.
+	 */
 	@Deprecated
 	@SuppressWarnings("deprecation")
 	protected Window(long ptr) {
@@ -66,12 +45,13 @@ public class Window extends SFMLNativeObject {
 	}
 
 	/**
-	 * Creates a new window.
+	 * Constructs a new window and creates it with the specified settings.
 	 *
-	 * @param mode     The video mode to use for OpenGL rendering.
-	 * @param title    The window title.
-	 * @param style    The window style.
-	 * @param settings The settings for the OpenGL context.
+	 * @param mode     the window's video mode.
+	 * @param title    the window title.
+	 * @param style    the window style.
+	 * @param settings the settings for the OpenGL context.
+	 * @see #create(VideoMode, String, int, ContextSettings)
 	 */
 	public Window(@NotNull VideoMode mode, @NotNull String title, int style, @NotNull ContextSettings settings) {
 		this();
@@ -79,11 +59,12 @@ public class Window extends SFMLNativeObject {
 	}
 
 	/**
-	 * Creates a new window with default context settings.
+	 * Constructs a new window and creates it with the specified settings and default
+	 * context settings..
 	 *
-	 * @param mode  The video mode to use for OpenGL rendering.
-	 * @param title The window title.
-	 * @param style The window style.
+	 * @param mode  the window's video mode.
+	 * @param title the window title.
+	 * @param style the window style.
 	 */
 	public Window(@NotNull VideoMode mode, @NotNull String title, int style) {
 		this();
@@ -91,14 +72,15 @@ public class Window extends SFMLNativeObject {
 	}
 
 	/**
-	 * Creates a new window with default style and context settings.
+	 * Constructs a new window and creates it with the specified settings and default
+	 * context settings and window style.
 	 *
-	 * @param mode  The video mode to use for rendering.
-	 * @param title The window title.
+	 * @param mode  the window's video mode.
+	 * @param title the window title.
 	 */
 	public Window(@NotNull VideoMode mode, @NotNull String title) {
 		this();
-		create(mode, title, DEFAULT, new ContextSettings());
+		create(mode, title, WindowStyle.DEFAULT, new ContextSettings());
 	}
 
 	@Override
@@ -119,33 +101,31 @@ public class Window extends SFMLNativeObject {
 	private native void nativeCreate(VideoMode mode, String title, int style, ContextSettings settings);
 
 	/**
-	 * Checks whether the current native thread is eligibile for spawning a window.
+	 * Checks whether the current native thread is eligibile for creating a window.
 	 * <p/>
 	 * This will always be the case on Windows or Linux, but on Mac OS X, it will check whether
 	 * the JVM was started in the main thread using the {@code -XstartOnFirstThread} command
 	 * line parameter.
 	 *
-	 * @return {@code true} if the current native thread may create a window, {@code false}
-	 *         otherwise.
+	 * @return {@code true} if the current native thread may create a window,
+	 *         {@code false} otherwise.
 	 */
 	public static native boolean isLegalWindowThread();
 
 	/**
-	 * Creates a window or re-creates it if it was already opened.
-	 * <p/>
-	 * Note that this method may raise a {@link JSFMLError} on Mac OS X systems if the thread
-	 * this method was invoked in is not allowed to open a window. See {@link #isLegalWindowThread()}
-	 * for more information.
+	 * Creates and opens a window or re-creates it if it was already opened.
 	 *
-	 * @param mode     The video mode to use for the OpenGL context. This must be a valid video mode in case
-	 *                 {@link Window#FULLSCREEN} is set.
-	 * @param title    The window title.
-	 * @param style    The style of the window. This should be a ORed combination of style flags
-	 *                 {@link Window#TITLEBAR}, {@link Window#RESIZE}, {@link Window#CLOSE} and
-	 *                 {@link Window#FULLSCREEN}, or {@link Window#NONE} for no style. {@link Window#DEFAULT}
-	 *                 provides a default combination of style flags.
-	 * @param settings The context settings to be applied to the context.
+	 * @param mode     the video mode that determines the window's size.
+	 *                 This must be a valid video mode in case {@link WindowStyle#FULLSCREEN} is set.
+	 * @param title    the window title.
+	 * @param style    the style of the window.
+	 *                 This should be a combination (using {@code OR} of the style flags
+	 *                 {@link WindowStyle#TITLEBAR}, {@link WindowStyle#RESIZE}, {@link WindowStyle#CLOSE} and
+	 *                 {@link WindowStyle#FULLSCREEN}, or {@link WindowStyle#NONE} for no style.
+	 *                 {@link WindowStyle#DEFAULT} provides a default combination of style flags.
+	 * @param settings the context settings for the created OpenGL context.
 	 * @see org.jsfml.window.VideoMode#isValid()
+	 * @see WindowStyle
 	 * @see ContextSettings
 	 */
 	public void create(@NotNull VideoMode mode, @NotNull String title, int style, @NotNull ContextSettings settings) {
@@ -165,62 +145,72 @@ public class Window extends SFMLNativeObject {
 		}
 
 		if ((style & FULLSCREEN) != 0 && !mode.isValid())
-			throw new IllegalArgumentException("Invalid video mode for fullscreen window.");
+			throw new IllegalArgumentException("Invalid video mode for a fullscreen window.");
 
 		nativeCreate(mode, title, style, settings);
 	}
 
 	/**
-	 * Creates a window or re-creates it if it was already opened with default context settings.
+	 * Creates and opens a window or re-creates it if it was already opened.
+	 * The default context settings will be used for the OpenGL context.
 	 *
-	 * @param mode  The video mode to use for the OpenGL context. This must be a valid video mode in case
-	 *              {@link Window#FULLSCREEN} is set.
-	 * @param title The window title.
-	 * @param style The style of the window.
+	 * @param mode  the video mode that determines the window's size.
+	 *              This must be a valid video mode in case {@link WindowStyle#FULLSCREEN} is set.
+	 * @param title the window title.
+	 * @param style the style of the window.
+	 *              This should be a combination (using {@code OR} of the style flags
+	 *              {@link WindowStyle#TITLEBAR}, {@link WindowStyle#RESIZE}, {@link WindowStyle#CLOSE} and
+	 *              {@link WindowStyle#FULLSCREEN}, or {@link WindowStyle#NONE} for no style.
+	 *              {@link WindowStyle#DEFAULT} provides a default combination of style flags.
 	 * @see org.jsfml.window.VideoMode#isValid()
+	 * @see WindowStyle
 	 */
 	public final void create(@NotNull VideoMode mode, @NotNull String title, int style) {
 		create(mode, title, style, new ContextSettings());
 	}
 
 	/**
-	 * Creates a window or re-creates it if it was already opened with default context settings and style
-	 * ({@link Window#DEFAULT}.
+	 * Creates and opens a window or re-creates it if it was already opened.
+	 * The {@link WindowStyle#DEFAULT} window style will be applied and default
+	 * context settings will be used for the OpenGL context.
 	 *
-	 * @param mode  The video mode to use for the OpenGL context.
-	 * @param title The window title.
+	 * @param mode  the video mode that determines the window's size.
+	 * @param title the window title.
 	 */
 	public final void create(@NotNull VideoMode mode, @NotNull String title) {
 		create(mode, title, DEFAULT, new ContextSettings());
 	}
 
 	/**
-	 * Closes the window and destroys all attached resources.
+	 * Closes the window and destroys all attached resources,
+	 * including the OpenGL context provided by it.
 	 */
 	public native void close();
 
 	/**
 	 * Checks if the window has been created and is opened.
 	 * <p/>
-	 * The user can modify this property by clicking the window's close button.
+	 * Note that the <i>open</i> state is not directly affected by the user clicking
+	 * the window's <i>close</i> button, if available. In order to accomplish that,
+	 * listen to an event of type {@link Event.Type#CLOSED} and use the {@link #close()} method.
 	 *
-	 * @return {@code true} if the window has been created and is currently opened.
+	 * @return {@code true} if the window has been created and is currently open.
 	 */
 	public native boolean isOpen();
 
 	/**
-	 * Gets the position of the window on the screen.
+	 * Gets the absolute position of the window's top left corner on the screen.
 	 *
-	 * @return The position of the window on the screen.
+	 * @return the absolute position of the window's top left corner on the screen.
 	 */
 	public native Vector2i getPosition();
 
 	protected native void nativeSetPosition(Vector2i v);
 
 	/**
-	 * Sets the position of the window on the screen.
+	 * Sets the absolute position of the window's top left corner on the screen.
 	 *
-	 * @param position The position on the screen.
+	 * @param position the new absolute position of the window's top left corner on the screen.
 	 */
 	public void setPosition(@NotNull Vector2i position) {
 		if (position == null)
@@ -232,7 +222,7 @@ public class Window extends SFMLNativeObject {
 	/**
 	 * Gets the size of the window.
 	 *
-	 * @return The size of the window.
+	 * @return the size of the window.
 	 */
 	public native Vector2i getSize();
 
@@ -241,7 +231,7 @@ public class Window extends SFMLNativeObject {
 	/**
 	 * Sets the size of the window.
 	 *
-	 * @param size The new size of the window.
+	 * @param size the new size of the window.
 	 */
 	public void setSize(@NotNull Vector2i size) {
 		if (size == null)
@@ -251,25 +241,28 @@ public class Window extends SFMLNativeObject {
 	}
 
 	/**
-	 * Retrieves the context settings of the window's rendering context.
+	 * Retrieves the context settings for the window's OpenGL context.
 	 *
-	 * @return The context settings of the window's rendering context.
+	 * @return the context settings for the window's OpenGL context.
 	 */
 	public native ContextSettings getSettings();
 
 	/**
 	 * Pops the event on top of the event stack, if any, and returns it.
+	 * <p/>
+	 * This method needs to be called regularly in order to process pending events. If this is
+	 * not done, the window will be unresponsive.
 	 *
-	 * @return The event currently on top of the event stack, or {@code null} if there is none.
+	 * @return the event currently on top of the event stack, or {@code null} if there is none.
 	 * @see #waitEvent()
 	 */
 	public native Event pollEvent();
 
 	/**
 	 * Returns an {@link Iterable} that consecutively calls {@link #pollEvent()} and
-	 * can be used to iterate over all events that occured since the last check.
+	 * can be used to process all pending events.
 	 *
-	 * @return An iterable over the received events.
+	 * @return an {@code Iterable} over all pending events.
 	 * @see #pollEvent()
 	 */
 	@JSFML
@@ -302,12 +295,12 @@ public class Window extends SFMLNativeObject {
 	}
 
 	/**
-	 * Pops the event on top of the event stack and returns it, or waits until an event occurs and then
-	 * returns it.
+	 * Pops the event on top of the event stack and returns it, or, if there is none,
+	 * waits until an event occurs and then returns it.
 	 * <p/>
-	 * Note that this method will block the program flow until an event is returned.
+	 * This method will block the program flow until an event is returned.
 	 *
-	 * @return The event currently on top of the event stack, or the next event that will occur.
+	 * @return the event currently on top of the event stack, or the next event that will occur.
 	 * @see #pollEvent()
 	 */
 	public native Event waitEvent();
@@ -315,10 +308,16 @@ public class Window extends SFMLNativeObject {
 	/**
 	 * Enables or disables vertical synchronization (VSync).
 	 * <p/>
-	 * Note that this should not be used in combination with {@link #setFramerateLimit(int)}
-	 * as these two will conflict with each other.
+	 * Activating vertical synchronization will limit the number of frames displayed
+	 * to the refresh rate of the monitor. This can avoid some visual artifacts,
+	 * and limit the framerate to a good value (but not constant across different computers).
+	 * <p/>
+	 * This should not be used in combination with {@link #setFramerateLimit(int)},
+	 * as these two will conflict with one another.
+	 * <p/>
+	 * By default, vertical synchronization is disabled.
 	 *
-	 * @param enable {@code true} to enable, {@code false} to disable.
+	 * @param enable {@code true} to enable vertical synchronization, {@code false} to disable.
 	 */
 	public native void setVerticalSyncEnabled(boolean enable);
 
@@ -329,12 +328,12 @@ public class Window extends SFMLNativeObject {
 	 */
 	public native void setMouseCursorVisible(boolean show);
 
-	protected native void nativeSetTitle(String title);
+	private native void nativeSetTitle(String title);
 
 	/**
 	 * Sets the window's title.
 	 *
-	 * @param title The window's new title.
+	 * @param title the window's new title.
 	 */
 	public void setTitle(@NotNull String title) {
 		if (title == null)
@@ -353,8 +352,10 @@ public class Window extends SFMLNativeObject {
 	/**
 	 * Determines whether automatic key repeat is enabled.
 	 * <p/>
-	 * If enabled, new key events will be fired when a key stays pressed (much like in a text field). This is
-	 * enabled by default.
+	 * If enabled, multiple key press events will be fired when a key stays pressed
+	 * (much like in a text field).
+	 * <p/>
+	 * Key repeat is enabled by default.
 	 *
 	 * @param enable {@code true} to enable, {@code false} to disabled.
 	 */
@@ -365,7 +366,7 @@ public class Window extends SFMLNativeObject {
 	/**
 	 * Sets the icon of the window.
 	 *
-	 * @param icon The icon.
+	 * @param icon the icon image.
 	 */
 	public void setIcon(@NotNull Image icon) {
 		if (icon == null)
@@ -380,10 +381,11 @@ public class Window extends SFMLNativeObject {
 	/**
 	 * Activates or deactivates the window as the current OpenGL rendering target.
 	 * <p/>
-	 * Note that if a window gets activated, all other windows operating in the same thread will
+	 * If a window gets activated, all other windows operating in the same thread will
 	 * automatically be deactivated.
 	 *
 	 * @param active {@code true} to activate, {@code false} to deactivate.
+	 * @throws ContextActivationException in case window activation fails.
 	 */
 	public void setActive(boolean active) throws ContextActivationException {
 		if (!nativeSetActive(active)) {
@@ -396,8 +398,10 @@ public class Window extends SFMLNativeObject {
 	/**
 	 * Activates the window as the current OpenGL rendering target.
 	 * <p/>
-	 * Note that if a window gets activated, all other windows operating in the same thread will
+	 * If a window gets activated, all other windows operating in the same thread will
 	 * automatically be deactivated.
+	 *
+	 * @throws ContextActivationException in case window activation fails.
 	 */
 	public final void setActive() throws ContextActivationException {
 		setActive(true);
@@ -406,27 +410,36 @@ public class Window extends SFMLNativeObject {
 	/**
 	 * Flushes the OpenGL pixel buffer to the screen.
 	 * <p/>
-	 * This should be called every frame to visibly update the scene.
+	 * This should be called every frame after everything has been drawn in order to
+	 * make the changes visible in the window.
 	 */
 	public native void display();
 
 	/**
 	 * Sets the maximum frame rate in frames per second.
 	 * <p/>
-	 * Note that this should not be used in combination with
-	 * {@link #setVerticalSyncEnabled(boolean)} } as these two will conflict with each other.
+	 * If a limit is set, the {@link #display()} method will block for a short delay time
+	 * after flushing the buffer in order to possibly maintain a constant frame rate.
+	 * <p/>
+	 * This should not be used in combination with
+	 * {@link #setVerticalSyncEnabled(boolean)} } as these two will conflict with one another.
+	 * <p/>
+	 * By default, there is no frame rate limit.
 	 *
-	 * @param flimit The maximum frame rate in frames per second.
+	 * @param flimit the maximum frame rate in frames per second, or {@code 0} to disable
+	 *               the frame rate limit.
 	 */
 	public native void setFramerateLimit(int flimit);
 
 	/**
 	 * Sets the joystick treshold.
 	 * <p/>
-	 * Joystick axis movements with a magnitude smaller than this treshold will not fire a joystick event.
+	 * Joystick axis movements with a magnitude smaller than this treshold will not
+	 * fire a joystick event.
+	 * <p/>
 	 * The default joystick treshold is 0.1.
 	 *
-	 * @param treshold The joystick treshold in a range between 0 and 100.
+	 * @param treshold the joystick treshold, ranging between 0 and 100.
 	 * @see org.jsfml.window.event.JoystickMoveEvent
 	 */
 	public native void setJoystickTreshold(float treshold);
