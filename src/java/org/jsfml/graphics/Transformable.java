ca462ddd6f72898e318ed0ef658fb125839d71f7
Patrick Dinklage <pdinklag@googlemail.com>
2012-12-25 05:12:17 +0100

    Improved Javadoc in Drawable and Transformable interface.
    Signed-off-by: Patrick Dinklage <pdinklag@googlemail.com>

--- src/java/org/jsfml/graphics/Transformable.java
+++ src/java/org/jsfml/graphics/Transformable.java
@@ -3,138 +3,140 @@ package org.jsfml.graphics;
 import org.jsfml.system.Vector2f;
 
 /**
- * Interface for transformable objects that can be positioned, rotated and scaled around an
- * origin.
+ * Interface for transformable objects that can be positioned in the scene and
+ * rotated and scaled around an origin.
+ * <p/>
+ * An implementation of this interface is provided by the {@link BasicTransformable} class.
  */
 public interface Transformable {
-    /**
-     * Sets the position of this object so that its origin will be exactly on it.
-     *
-     * @param x The X coordinate.
-     * @param y The Y coordinate.
-     */
-    public void setPosition(float x, float y);
+	/**
+	 * Sets the position of this object in the scene so that its origin will be exactly on it.
+	 *
+	 * @param x the new X coordinate.
+	 * @param y the new Y coordinate.
+	 */
+	public void setPosition(float x, float y);
 
-    /**
-     * Sets the position of this object so that its origin will be exactly on it.
-     *
-     * @param v The new position.
-     */
-    public void setPosition(Vector2f v);
+	/**
+	 * Sets the position of this object in the scene so that its origin will be exactly on it.
+	 *
+	 * @param v the new position.
+	 */
+	public void setPosition(Vector2f v);
 
-    /**
-     * Sets the rotation of this object around its origin.
-     *
-     * @param angle The new rotation angle in degrees.
-     */
-    public void setRotation(float angle);
+	/**
+	 * Sets the rotation of this object around its origin.
+	 *
+	 * @param angle the new rotation angle in degrees.
+	 */
+	public void setRotation(float angle);
 
-    /**
-     * Sets the scaling of this object, using its origin as the scaling center.
-     *
-     * @param x The X scaling factor.
-     * @param y The Y scaling factor.
-     */
-    public void setScale(float x, float y);
+	/**
+	 * Sets the scaling of this object, using its origin as the scaling center.
+	 *
+	 * @param x the new X scaling factor.
+	 * @param y the new Y scaling factor.
+	 */
+	public void setScale(float x, float y);
 
-    /**
-     * Sets the scaling of this object, using its origin as the scaling center.
-     *
-     * @param factors The new scaling factors.
-     */
-    public void setScale(Vector2f factors);
+	/**
+	 * Sets the scaling of this object, using its origin as the scaling center.
+	 *
+	 * @param factors the new scaling factors.
+	 */
+	public void setScale(Vector2f factors);
 
-    /**
-     * Sets the rotation, scaling and drawing origin of this object.
-     *
-     * @param x The X coordinate.
-     * @param y The Y coordinate.
-     */
-    public void setOrigin(float x, float y);
+	/**
+	 * Sets the rotation, scaling and drawing origin of this object.
+	 *
+	 * @param x the new X coordinate of the origin.
+	 * @param y the new Y coordinate of the origin.
+	 */
+	public void setOrigin(float x, float y);
 
-    /**
-     * Sets the rotation, scaling and drawing origin of this object.
-     *
-     * @param v The new origin.
-     */
-    public void setOrigin(Vector2f v);
+	/**
+	 * Sets the rotation, scaling and drawing origin of this object.
+	 *
+	 * @param v the new origin.
+	 */
+	public void setOrigin(Vector2f v);
 
-    /**
-     * Gets the position of this object.
-     *
-     * @return The current position.
-     */
-    public Vector2f getPosition();
+	/**
+	 * Gets the position of this object.
+	 *
+	 * @return the current position.
+	 */
+	public Vector2f getPosition();
 
-    /**
-     * Gets the rotation angle of this object.
-     *
-     * @return The current rotation angle in degrees.
-     */
-    public float getRotation();
+	/**
+	 * Gets the rotation angle of this object.
+	 *
+	 * @return the current rotation angle in degrees.
+	 */
+	public float getRotation();
 
-    /**
-     * Gets the scaling of this object.
-     *
-     * @return The current scaling factors.
-     */
-    public Vector2f getScale();
+	/**
+	 * Gets the scaling of this object.
+	 *
+	 * @return the current scaling factors.
+	 */
+	public Vector2f getScale();
 
-    /**
-     * Gets the origin of this object.
-     *
-     * @return The current origin.
-     */
-    public Vector2f getOrigin();
+	/**
+	 * Gets the origin of this object.
+	 *
+	 * @return the current origin.
+	 */
+	public Vector2f getOrigin();
 
-    /**
-     * Moves this object.
-     *
-     * @param x The X offset added to the current position.
-     * @param y The Y offset added to the current position.
-     */
-    public void move(float x, float y);
+	/**
+	 * Moves this object.
+	 *
+	 * @param x the X offset added to the current position.
+	 * @param y the Y offset added to the current position.
+	 */
+	public void move(float x, float y);
 
-    /**
-     * Moves the object.
-     *
-     * @param v The offset vector added to the current position.
-     */
-    public void move(Vector2f v);
+	/**
+	 * Moves the object.
+	 *
+	 * @param v the offset vector added to the current position.
+	 */
+	public void move(Vector2f v);
 
-    /**
-     * Rotates this object around its origin.
-     *
-     * @param angle The rotation angle in degrees.
-     */
-    public void rotate(float angle);
+	/**
+	 * Rotates this object around its origin.
+	 *
+	 * @param angle the rotation angle in degrees.
+	 */
+	public void rotate(float angle);
 
-    /**
-     * Scales the object, using its origin as the scaling center.
-     *
-     * @param x The X scaling factor.
-     * @param y The Y scaling factor.
-     */
-    public void scale(float x, float y);
+	/**
+	 * Scales the object, using its origin as the scaling center.
+	 *
+	 * @param x the X scaling factor.
+	 * @param y the Y scaling factor.
+	 */
+	public void scale(float x, float y);
 
-    /**
-     * Scales the object, using its origin as the scaling center.
-     *
-     * @param factors The scaling factors.
-     */
-    public void scale(Vector2f factors);
+	/**
+	 * Scales the object, using its origin as the scaling center.
+	 *
+	 * @param factors the scaling factors.
+	 */
+	public void scale(Vector2f factors);
 
-    /**
-     * Gets the current transform matrix.
-     *
-     * @return The current transform.
-     */
-    public Transform getTransform();
+	/**
+	 * Gets the current transformation matrix.
+	 *
+	 * @return the current transformation.
+	 */
+	public Transform getTransform();
 
-    /**
-     * Gets the inverse of the current transform matrix.
-     *
-     * @return The inverse of the current transform.
-     */
-    public Transform getInverseTransform();
+	/**
+	 * Gets the inverse of the current transformation matrix.
+	 *
+	 * @return the inverse of the current transform.
+	 */
+	public Transform getInverseTransform();
 }
